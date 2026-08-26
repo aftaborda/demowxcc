5d0184bb0c26d88220a46edfbaa271aa3bb82de2
aftaborda <antonio.taborda@warpcom.com>
2026-08-26 17:58:12 +0100

    Update app.js

--- app.js
+++ app.js
@@ -4,58 +4,81 @@ const users = {
     id: "1000",
     name: "Joaquim Santos",
     email: "joaquim.santos@mail.local",
+    phone: "+351900111222",
+    language: "pt-PT",
   },
   1001: {
     id: "1001",
     name: "Genoveva Pascoal",
     email: "genoveva.p@mail.local",
+    phone: "+351966111333",
+    language: "pt-PT",
   },
   1002: {
     id: "1002",
     name: "Hermenegildo Balsemão",
     email: "hermenegildo.b@mail.local",
+    phone: "+351910001122",
+    language: "pt-PT",
   },
   1003: {
     id: "1003",
     name: "Teotónio Pires-Veloso",
     email: "teotonio.pv@mail.local",
+    phone: "+351966000111",
+    language: "pt-PT",
   },
   1004: {
     id: "1004",
     name: "Eulália Quaresma",
     email: "eulalia.q@mail.local",
+    phone: "+351910555444",
+    language: "en-GB",
   },
   1005: {
     id: "1005",
     name: "Zulmira Labareda",
     email: "zulmira.l@mail.local",
+    phone: "+351220777888",
+    language: "en-GB",
   },
   1006: {
     id: "1006",
     name: "Floripes Incarnação",
     email: "floripes.i@mail.local",
+    phone: "+351922888999",
+    language: "en-GB",
   },
   1007: {
     id: "1007",
     name: "Custódio Melancia",
     email: "custodio.m@mail.local",
+    phone: "+351933777666",
+    language: "en-GB",
   },
   1008: {
     id: "1008",
     name: "Anacleto Riba-Tua",
     email: "anacleto.r@mail.local",
+    phone: "+351210444555",
+    language: "es-ES",
   },
   1009: {
     id: "1009",
     name: "Olegário Boavida",
     email: "olegario.b@mail.local",
+    phone: "+351922666777",
+    language: "es-ES",
   },
   1010: {
     id: "1010",
     name: "Sidónia Castanheira",
     email: "sidonia.c@mail.local",
+    phone: "+351933222111",
+    language: "es-ES",
   },
 };
+
 
 const loginForm = document.getElementById("loginForm");
 
@@ -192,7 +215,16 @@ if (!loginForm) {
     if (!user) return;
 
     console.log(`[Custom Integration] update widget for user ${user.id}`);
-    const data = `{"custom_chat_fields": {"customerName": "${user.name}", "customerEmail": "${user.email}", "customerId": "${user.id}"}}`;
+    const custom_chat_fields = {
+      userId: user.id,
+      userName: user.name,
+      userEmail: user.email,
+      userPhone: user.phone,
+      userLanguage: user.language,
+    };
+    //const data = `{"custom_chat_fields": {"customerName": "${user.name}", "customerEmail": "${user.email}", "customerId": "${user.id}"}}`;
+    const data = JSON.stringify(custom_chat_fields);
+    console.log(data);
     window.imichatwidget.update(data, function (response) {
       console.log(
         `[Custom Integration] User id: ${user.id}, Resposta do widget update: ${JSON.stringify(response)}`,
